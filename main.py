4d64ec2a5fd5ec974722404a76c56741c5ada61e
root <root@Maximus.localdomain>
2019-04-11 15:33:14 -0400

    testing out feature array conversion, working but just a save point

--- main.py
+++ main.py
@@ -10,16 +10,50 @@ from string import ascii_uppercase
 def main():
 
 
-    get_alphabet()
-    # Makes window for letter a
+    #get_alphabet()
 
+    
+    for i in range(0,40,4):
+        print(i)
+        for j in range(0,20,4):
+            # Top left square
+            print(i,j)
+            print(i,j+1)
+            print(i+1, j)
+            print(i+1, j+1)
+
+            # Bottom left square
+            print(i+2,j)
+            print(i+2,j+1)
+            print(i+3, j)
+            print(i+3, j+1)
+
+            # Top right square
+            print(i,j+2)
+            print(i,j+3)
+            print(i+1, j+2)
+            print(i+1, j+3)
+
+            # Bottom right sqaure
+            print(i+2,j+2)
+            print(i+2,j+3)
+            print(i+3, j+2)
+            print(i+3, j+3)
+            print("endSqaure\n")
+
+
+        
+
+    
+            
     t = Letter_Window("b")
     t.mainloop()
 
     u = User_Window()
     u.mainloop()
-    
 
+
+    
 
 def get_alphabet():
 
