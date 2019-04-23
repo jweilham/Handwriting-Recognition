558d06cad961e0c8b28f70682524e7158213ad2e
jweilham <jweilham@iu.edu>
2019-04-22 22:25:40 -0400

    added knn and neural net to User_Window

--- User_Window.py
+++ User_Window.py
@@ -1,6 +1,8 @@
 from Window import *
 import time
 from Neural_Network import*
+from sklearn.neighbors import KNeighborsClassifier
+from string import ascii_lowercase
 
 class User_Window(Window):
 
@@ -16,12 +18,82 @@ class User_Window(Window):
         self.net = Neural_Network()
         self.net.load()
 
+        #for K nearest Neighbors
+        self.data = []
+        self.test_data = []
+        self.knn = KNeighborsClassifier()
+        self.load_knn()
+
+        
     def saveq(self):
         
         self.save("user_input/" + self.filename)
         self.display()
 
 
+    def load_knn(self):
+
+
+        self.loadee()
+        labels = [0]*416
+        counter = 0
+        let = 0
+        for i in range(len(labels)):
+            labels[i] = let
+            counter += 1
+
+            if(not(counter%16)):
+                let += 1
+
+        plz = []
+        test = []
+        count = 0
+        for i in self.data:
+            for j in range(len(i)-2):
+                #print(j)
+                plz.append(i[j])
+
+            test.append(self.test_data[count])
+            
+            count +=1
+
+
+        self.knn.fit(plz, labels)
+        KNeighborsClassifier(algorithm='auto', 
+                     leaf_size=30, 
+                     metric='minkowski',
+                     metric_params=None, 
+                     n_jobs=1, 
+                     n_neighbors=5, 
+                     p=2,
+                     weights='uniform')
+        
+        #print("Predictions form the classifier:")
+        #print(self.knn.predict(test))
+        #print("Target values:")
+        #rint([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25])
+
+
+        
+    def loader(self, letter):
+
+        print("loader!")
+        l = []
+        for key in letter:
+            l.append(letter[key])
+
+        #print(len(l))
+        self.data.append(l)
+        #print("loading => ", l[16])
+        self.test_data.append(l[16])
+        #print("test_data: ", self.test_data)
+
+    def loadee(self):
+        
+        for i in ascii_lowercase:
+            print("loading: ", i)
+            letter = np.load("./data/features/" + i + ".npz")
+            self.loader(letter)
     def last(self):
         
         self.display()
@@ -84,6 +156,37 @@ class User_Window(Window):
             
             x,y,w,h = cv2.boundingRect(contours[i])
      
+            #cv2.rectangle(copy,         # Draw rectangle on temporary copy
+             #            (x, y),        # Start
+              #           (x+w,y+h),     # End
+               #          (255, 0, 0),   # BGR value (RGB backwards)
+                #          2)            # Thickness
+
+            imgROI = image[y:y+h, x:x+w]
+
+            #cv2.imshow("hello", imgROI)
+            #cv2.waitKey(0)
+            #cv2.destroyAllWindows()
+            unsorted_list.append([x + (y+h)*5,self.get_feature(imgROI),contours[i]])
+
+            #self.net.print(self.get_feature(imgROI))
+
+
+        
+        unsorted_list.sort(key = lambda x: x[0])
+
+
+
+        
+            
+
+
+        for i in unsorted_list:
+            print(i[0])
+            self.net.print(i[1])
+            print("knn thinkgs it's a: ", ascii_lowercase[int(self.knn.predict([i[1]])[0])])
+            x,y,w,h = cv2.boundingRect(i[2])
+     
             cv2.rectangle(copy,         # Draw rectangle on temporary copy
                          (x, y),        # Start
                          (x+w,y+h),     # End
@@ -95,24 +198,8 @@ class User_Window(Window):
             cv2.imshow("hello", imgROI)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
-            unsorted_list.append([x + ((y*h)/2)*7,self.get_feature(imgROI)])
-
-            self.net.print(self.get_feature(imgROI))
-
-
-        
-        unsorted_list.sort(key = lambda x: x[0])
-
-        
             
-
-
-        for i in unsorted_list:
-            print(i[0])
-            self.net.print(i[1])
-
-        #list = [9575.0, 4965.0, 4210.5, 4694.0]
-
+            
             
         #self.destroy()
         cv2.imshow("user_input", copy)
--- main.py
+++ main.py
@@ -6,7 +6,11 @@ import letter_detection     # detecting i's and j's
 from Letter_Window import *
 from User_Window import *
 from string import ascii_lowercase
+# Create and fit a nearest-neighbor classifier
+from sklearn.neighbors import KNeighborsClassifier
 
+data = []
+test_data = []
 
 def alphabet():
 
@@ -17,11 +21,68 @@ def alphabet():
         j = 0
         for key in load:
             data.append(load[key])
-            print(i, data[j])
+            #print(i, data[j])
             j+=1
 
 
+
+def loader(letter):
+    l = []
+    for key in letter:
+        l.append(letter[key])
+
+    #print(len(l))
+    data.append(l)
+    test_data.append(l[16])
+
+def loadee():
+    
+    for i in ascii_lowercase:
+        print("loading: ", i)
+        letter = np.load("./data/features/" + i + ".npz")
+        loader(letter)
+
 def main():
+
+    labels = [0]*416
+    counter = 0
+    let = 0
+    for i in range(len(labels)):
+        labels[i] = let
+        counter += 1
+
+        if(not(counter%16)):
+            let += 1
+
+    loadee()
+
+    plz = []
+    test = []
+    count = 0
+    for i in data:
+        for j in range(len(i)-2):
+            #print(j)
+            plz.append(i[j])
+
+        test.append(test_data[count])
+        count +=1
+
+    print(labels)
+        
+    knn = KNeighborsClassifier()
+    knn.fit(plz, labels) 
+    KNeighborsClassifier(algorithm='auto', 
+                         leaf_size=30, 
+                         metric='minkowski',
+                         metric_params=None, 
+                         n_jobs=1, 
+                         n_neighbors=5, 
+                         p=2,
+                         weights='uniform')
+    print("Predictions form the classifier:")
+    print(knn.predict(test))
+    print("Target values:")
+    print([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 
     #alphabet()
 
